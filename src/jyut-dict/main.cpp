fc1044e713d0e94887efb802b44a48c12a01e6f3
Aaron Tan <hi@aaronhktan.com>
2019-01-22 21:28:03 -0500

    Support HiDPI in Windows
    - QApplication::setAttribute(Qt::AA_EnableHighDpiScaling) must be set *before* QApplication is instantiated.

--- src/jyut-dict/main.cpp
+++ src/jyut-dict/main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
-    a.setAttribute(Qt::AA_EnableHighDpiScaling);
-    a.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
     MainWindow w;
     w.show();
