66639e9924ff5857f702202380757362c6caad0e
Aaron Tan <hi@aaronhktan.com>
2019-04-03 01:25:56 -0400

    Change minimum window size only for Ubuntu

--- src/jyut-dict/windows/mainwindow.cpp
+++ src/jyut-dict/windows/mainwindow.cpp
@@ -8,7 +8,11 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
     setWindowTitle("Jyut Dictionary");
+#ifdef Q_OS_LINUX
     setMinimumSize(QSize(600, 600));
+#else
+    setMinimumSize(QSize(800, 600));
+#endif
 
     _mainToolBar = new MainToolBar(this);
     addToolBar(_mainToolBar);
