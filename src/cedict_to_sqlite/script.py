097102c85ab8d4a4b24dd38fa30b7ac86ad27d73
Aaron Tan <hi@aaronhktan.com>
2019-07-16 01:43:52 -0400

    WIP: Use new database schema
    - Split up massive database into three smaller ones:
      - Entries, containing entries in their representations (simp., trad., pinyin, jyutping)
      - Definitions, containing definitions, with foreign keys pointing to an entry and a source
      - Sources, with source names

--- src/cedict_to_sqlite/script.py
+++ src/cedict_to_sqlite/script.py
@@ -3,8 +3,10 @@ from wordfreq import zipf_frequency
 import sqlite3
 import sys
 
+sources = ("CC_CEDICT", "CC_CANTO")
+
 class Entry(object):
-    def __init__(self, trad='', simp='', pin='', jyut='', freq=0.0, cedict_eng='', canto_eng=''):
+    def __init__(self, trad='', simp='', pin='', jyut='', freq=0.0, cedict_eng=[], canto_eng=[]):
         self.traditional = trad
         self.simplified = simp
         self.pinyin = pin
@@ -26,14 +28,88 @@ def write(entries, db_name):
     db = sqlite3.connect(db_name)
     c = db.cursor()
 
-    c.execute('drop table if exists entries');
-    c.execute('create virtual table entries using fts5(traditional, simplified, pinyin, jyutping, freq, cedict_english, canto_english)')
+    # Create tables
+    c.execute('DROP TABLE IF EXISTS entries')
+    c.execute('DROP TABLE IF EXISTS entries_fts')
+    c.execute('DROP TABLE IF EXISTS sources')
+    c.execute('DROP TABLE IF EXISTS definitions')
+    c.execute('DROP TABLE IF EXISTS definitions_fts')
 
+    c.execute('''CREATE TABLE entries(
+                    entry_id INTEGER PRIMARY KEY,
+                    traditional TEXT,
+                    simplified TEXT,
+                    pinyin TEXT,
+                    jyutping TEXT,
+                    frequency REAL
+                )''')
+    c.execute('CREATE VIRTUAL TABLE entries_fts using fts5(pinyin, jyutping)')
+
+    c.execute('''CREATE TABLE sources(
+                    source_id INTEGER PRIMARY KEY,
+                    sourcename TEXT
+                )''')
+
+    c.execute('''CREATE TABLE definitions(
+                    definition_id INTEGER PRIMARY KEY,
+                    definition TEXT,
+                    fk_entry_id INTEGER,
+                    fk_source_id INTEGER,
+                    FOREIGN KEY(fk_entry_id) REFERENCES entries(entry_id) ON UPDATE CASCADE,
+                    FOREIGN KEY(fk_source_id) REFERENCES sources(source_id) ON DELETE CASCADE
+                )''')
+    c.execute('CREATE VIRTUAL TABLE definitions_fts using fts5(definition)')
+
+    # SQLITE 3 currently only supports triggers FOR EACH ROW
+    # making these extremely slow and time-consuming
+    # Disable for now.
+    # Delete entries when no definitions reference them
+    # c.execute('''CREATE TRIGGER IF NOT EXISTS entry_cleanup 
+    #                 AFTER DELETE ON definitions
+    #             BEGIN
+    #                 DELETE FROM entries WHERE entry_id NOT IN (SELECT fk_entry_id FROM definitions);
+    #             END
+    #             ''')
+
+    # Rebuild entries FTS after modifying entries
+    # c.execute('''CREATE TRIGGER IF NOT EXISTS entries_fts_cleanup 
+    #                 AFTER DELETE ON entries
+    #             BEGIN
+    #                 DELETE FROM entries_fts WHERE rowid NOT IN (SELECT entry_id FROM entries);
+    #             END
+    #             ''')
+
+    # Rebuild definition FTS after modifying definitions
+    # c.execute('''CREATE TRIGGER IF NOT EXISTS definitions_fts_cleanup 
+    #                 AFTER DELETE ON definitions
+    #             BEGIN
+    #                 DELETE FROM definitions_fts WHERE rowid NOT IN (SELECT definition_id FROM definitions);
+    #             END
+    #             ''')
+
+    # Add sources to tables
+    [c.execute('INSERT INTO sources values(?,?)', (None, value)) for value in sources]
+
+    # Add entries to tables
     def entry_to_tuple(entry):
-        return (entry.traditional, entry.simplified, entry.pinyin, entry.jyutping, entry.freq, entry.cedict_english, entry.canto_english)
+        return (None, entry.traditional, entry.simplified, entry.pinyin, entry.jyutping, entry.freq)
 
-    entry_tuples = map(entry_to_tuple, entries.values())
-    c.executemany('insert into entries values (?,?,?,?,?,?,?)', entry_tuples)
+    def definition_to_tuple(definition, entry_id, source_id):
+        return (None, definition, entry_id, source_id)
+
+    for key in entries:
+        for entry in entries[key]:
+            c.execute('INSERT INTO entries values (?,?,?,?,?,?)', entry_to_tuple(entry))
+
+            c.execute('SELECT last_insert_rowid()')
+            entry_id = c.fetchone()[0]
+            definition_tuples = [definition_to_tuple(definition, entry_id, sources.index('CC_CEDICT')+1) for definition in entry.cedict_english]
+            definition_tuples += [definition_to_tuple(definition, entry_id, sources.index('CC_CANTO')+1) for definition in entry.canto_english]
+            c.executemany('INSERT INTO definitions values (?,?,?,?)', definition_tuples)
+
+    # Populate FTS versions of tables
+    c.execute('INSERT INTO entries_fts (rowid, pinyin, jyutping) SELECT rowid, pinyin, jyutping FROM entries')
+    c.execute('INSERT INTO definitions_fts (rowid, definition) SELECT rowid, definition FROM definitions')
 
     db.commit()
     db.close()
@@ -48,16 +124,16 @@ def parse_cc_cedict(filename, entries):
             trad = split[0]
             simp = split[1]
             pin = line[line.index('[') + 1 : line.index(']')]
-            eng = line[line.index('/') + 1 : -2]
+            eng = line[line.index('/') + 1 : -2].split('/')
             entry = Entry(trad=trad,
                           simp=simp,
                           pin=pin,
                           cedict_eng=eng)
 
             if trad in entries:
-                entries[trad + str(index)] = entry
+                entries[trad].append(entry)
             else:
-                entries[trad] = entry
+                entries[trad] = [entry]
 
 def parse_cc_canto(filename, entries):
     with open(filename, 'r') as f:
@@ -70,7 +146,7 @@ def parse_cc_canto(filename, entries):
             simp = split[1]
             pin = line[line.index('[') + 1 : line.index(']')]
             jyut = line[line.index('{') + 1 : line.index('}')]
-            eng = line[line.index('/') + 1 : -2]
+            eng = [line[line.index('/') + 1 : -2]]
             entry = Entry(trad=trad,
                           simp=simp,
                           pin=pin,
@@ -78,10 +154,11 @@ def parse_cc_canto(filename, entries):
                           canto_eng=eng)
 
             if trad in entries:
-                entries[trad].add_jyut_ping(jyut)
-                entries[trad].add_canto_eng(eng)
+                for entry in entries[trad]:
+                    entry.add_jyut_ping(jyut)
+                entries[trad][0].add_canto_eng(eng)
             else:
-                entries[trad] = entry
+                entries[trad] = [entry]
 
 def parse_cc_cedict_canto_readings(filename, entries):
     with open(filename, 'r') as f:
@@ -95,12 +172,14 @@ def parse_cc_cedict_canto_readings(filename, entries):
             if trad not in entries:
                 continue
 
-            entries[trad].add_jyut_ping(jyut)
+            for entry in entries[trad]:
+                entry.add_jyut_ping(jyut)
 
 def assign_frequencies(entries):
-    for entry in entries:
-        freq = zipf_frequency(entries[entry].traditional, 'zh')
-        entries[entry].add_freq(freq)
+    for key in entries:
+        for entry in entries[key]:
+            freq = zipf_frequency(entry.traditional, 'zh')
+            entry.add_freq(freq)
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
