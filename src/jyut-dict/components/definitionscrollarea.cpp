81f27b3b69cd6a76d3551ea205edf774ca7cb579
Aaron Tan <hi@aaronhktan.com>
2019-08-07 21:32:35 -0400

    Show scrollbar depending on context on macOS

--- src/jyut-dict/components/definitionscrollarea.cpp
+++ src/jyut-dict/components/definitionscrollarea.cpp
@@ -84,12 +84,8 @@ void DefinitionScrollArea::setEntry(const Entry &entry)
 }
 
 void DefinitionScrollArea::resizeEvent(QResizeEvent *event) {
-#if defined(Q_OS_WIN) || defined(Q_OS_LINUX)
     _scrollAreaWidget->resize(width()
                               - (verticalScrollBar()->isVisible() ? verticalScrollBar()->width() : 0),
                               _scrollAreaWidget->sizeHint().height());
-#else
-    _scrollAreaWidget->resize(width(), _scrollAreaWidget->sizeHint().height());
-#endif
     event->accept();
 }
