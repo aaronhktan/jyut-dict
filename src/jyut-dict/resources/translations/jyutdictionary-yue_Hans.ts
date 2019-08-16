a7f9a42cf4bde3a5ab760637c76d7161088a1d51
Aaron Tan <hi@aaronhktan.com>
2019-08-15 21:45:17 -0400

    Add translations for QMessageBox
    - For some reason, Qt doesn't seem to pick up on the translations for QMessageBox, so manually override them and add them in.
    - Fix some missing traditional characters in zh_Hant and zh_Hant_HK.

--- src/jyut-dict/resources/translations/jyutdictionary-yue_Hans.ts
+++ src/jyut-dict/resources/translations/jyutdictionary-yue_Hans.ts
@@ -2,6 +2,46 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="yue_CN">
 <context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../src/gui/dialogs/qmessagebox.cpp" line="-1111"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location line="-853"/>
+        <location line="+852"/>
+        <location filename="../src/gui/dialogs/qmessagebox.h" line="-52"/>
+        <location line="+8"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location line="+509"/>
+        <source>About Qt</source>
+        <translation>关于Qt</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;</source>
+        <translation type="obsolete">&lt;p&gt;这个程序使用的是Qt %1版。&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location line="-1605"/>
+        <source>Show Details...</source>
+        <translation>显示详细信息…</translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Hide Details...</source>
+        <translation>隐藏详细信息…</translation>
+    </message>
+    <message>
+        <location line="+1570"/>
+        <source>&lt;h3&gt;About Qt&lt;/h3&gt;&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;&lt;p&gt;Qt is a C++ toolkit for cross-platform application development.&lt;/p&gt;&lt;p&gt;Qt provides single-source portability across MS&amp;nbsp;Windows, Mac&amp;nbsp;OS&amp;nbsp;X, Linux, and all major commercial Unix variants. Qt is also available for embedded devices as Qt for Embedded Linux and Qt for Windows CE.&lt;/p&gt;&lt;p&gt;Qt is available under three different licensing options designed to accommodate the needs of our various users.&lt;/p&gt;Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of the GNU LGPL version 2.1 or GNU GPL version 3.0.&lt;/p&gt;&lt;p&gt;Qt licensed under the GNU LGPL version 2.1 is appropriate for the development of Qt applications (proprietary or open source) provided you can comply with the terms and conditions of the GNU LGPL version 2.1.&lt;/p&gt;&lt;p&gt;Qt licensed under the GNU General Public License version 3.0 is appropriate for the development of Qt applications where you wish to use such applications in combination with software subject to the terms of the GNU GPL version 3.0 or where you are otherwise willing to comply with the terms of the GNU GPL version 3.0.&lt;/p&gt;&lt;p&gt;Please see &lt;a href=&quot;http://qt.nokia.com/products/licensing&quot;&gt;qt.nokia.com/products/licensing&lt;/a&gt; for an overview of Qt licensing.&lt;/p&gt;&lt;p&gt;Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).&lt;/p&gt;&lt;p&gt;Qt is a Nokia product. See &lt;a href=&quot;http://qt.nokia.com/&quot;&gt;qt.nokia.com&lt;/a&gt; for more information.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>DictionaryTab</name>
     <message>
         <location filename="../../components/dictionarytab.cpp" line="31"/>
@@ -265,7 +305,7 @@
     <message>
         <location filename="../../components/resultlistmodel.cpp" line="38"/>
         <source>Start typing to search for words</source>
-        <translation>开始输入嚟查字典</translation>
+        <translation>开始输入并搜索</translation>
     </message>
 </context>
 <context>
@@ -412,7 +452,7 @@
     <message>
         <location filename="../../windows/updatewindow.cpp" line="68"/>
         <source>Show Details</source>
-        <translation>显示详细信息</translation>
+        <translation>显示详细信息…</translation>
     </message>
     <message>
         <location filename="../../windows/updatewindow.cpp" line="72"/>
@@ -422,12 +462,12 @@
     <message>
         <location filename="../../windows/updatewindow.cpp" line="100"/>
         <source>Hide details</source>
-        <translation>隐藏详细信息</translation>
+        <translation>隐藏详细信息…</translation>
     </message>
     <message>
         <location filename="../../windows/updatewindow.cpp" line="119"/>
         <source>Show details</source>
-        <translation>显示详细信息</translation>
+        <translation>显示详细信息…</translation>
     </message>
 </context>
 </TS>
--- src/jyut-dict/resources/translations/jyutdictionary-yue_Hant.ts
+++ src/jyut-dict/resources/translations/jyutdictionary-yue_Hant.ts
@@ -2,6 +2,42 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="yue_HK" sourcelanguage="en_CA">
 <context>
+    <name>QMessageBox</name>
+    <message>
+        <source>Show Details...</source>
+        <translation>顯示詳細資料…</translation>
+    </message>
+    <message>
+        <source>Hide Details...</source>
+        <translation>隱藏詳細資料…</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;About Qt&lt;/h3&gt;&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;關於 Qt&lt;/h3&gt;&lt;p&gt;此程式使用 Qt %1 版。&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Qt is a C++ toolkit for cross-platform application development.&lt;/p&gt;&lt;p&gt;Qt provides single-source portability across all major desktop operating systems. It is also available for embedded Linux and other embedded and mobile operating systems.&lt;/p&gt;&lt;p&gt;Qt is available under multiple licensing options designed to accommodate the needs of our various users.&lt;/p&gt;&lt;p&gt;Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of GNU (L)GPL.&lt;/p&gt;&lt;p&gt;Qt licensed under GNU (L)GPL is appropriate for the development of Qt&amp;nbsp;applications provided you can comply with the terms and conditions of the respective licenses.&lt;/p&gt;&lt;p&gt;Please see &lt;a href=&quot;http://%2/&quot;&gt;%2&lt;/a&gt; for an overview of Qt licensing.&lt;/p&gt;&lt;p&gt;Copyright (C) %1 The Qt Company Ltd and other contributors.&lt;/p&gt;&lt;p&gt;Qt and the Qt logo are trademarks of The Qt Company Ltd.&lt;/p&gt;&lt;p&gt;Qt is The Qt Company Ltd product developed as an open source project. See &lt;a href=&quot;http://%3/&quot;&gt;%3&lt;/a&gt; for more information.&lt;/p&gt;</source>
+        <extracomment>Leave this text untranslated or include a verbatim copy of it below and note that it is the authoritative version in case of doubt.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>關於 Qt</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Qt is a C++ toolkit for cross-platform application development.&lt;/p&gt;&lt;p&gt;Qt provides single-source portability across all major desktop operating systems. It is also available for embedded Linux and other embedded and mobile operating systems.&lt;/p&gt;&lt;p&gt;Qt is available under three different licensing options designed to accommodate the needs of our various users.&lt;/p&gt;&lt;p&gt;Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of the GNU LGPL version 3.&lt;/p&gt;&lt;p&gt;Qt licensed under the GNU LGPL version 3 is appropriate for the development of Qt&amp;nbsp;applications provided you can comply with the terms and conditions of the GNU LGPL version 3.&lt;/p&gt;&lt;p&gt;Please see &lt;a href=&quot;http://%2/&quot;&gt;%2&lt;/a&gt; for an overview of Qt licensing.&lt;/p&gt;&lt;p&gt;Copyright (C) %1 The Qt Company Ltd and other contributors.&lt;/p&gt;&lt;p&gt;Qt and the Qt logo are trademarks of The Qt Company Ltd.&lt;/p&gt;&lt;p&gt;Qt is The Qt Company Ltd product developed as an open source project. See &lt;a href=&quot;http://%3/&quot;&gt;%3&lt;/a&gt; for more information.&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Qt 是個用於跨平台應用程式開發的工具箱。&lt;/p&gt;
+&lt;p&gt;Qt 提供了可移植單一來源以能在目前主流的桌面作業系統跨平台。其也支援嵌入式 Linux 和其他嵌入和行動作業系統。&lt;/p&gt;
+&lt;p&gt;Qt 可以用於三個不同的授權選項，以接納各個使用者的需求。&lt;/p&gt;
+&lt;p&gt;使用我們的商業授權條款授權的 Qt 適合拿來開發不希望公開源碼給第三方的專有或商業軟體，或者是無法完全遵守 GNU LGPL 第三版條款的軟體。&lt;/p&gt;
+&lt;p&gt;使用 GNU LGPL 第三版授權的 Qt 適合拿來開發能遵守 LGPL 授權條款與條件的 Qt 應用程式。&lt;/p&gt;
+&lt;p&gt;請參閱 &lt;a href=&quot;http://%2/&quot;&gt;%2&lt;/a&gt; 取得 Qt 授權概覽。&lt;/p&gt;
+&lt;p&gt;版權所有 (C) %1 Qt 公司與其他貢獻者。&lt;/p&gt;
+&lt;p&gt;Qt 與其圖示皆為 Qt 公司的商標。&lt;/p&gt;
+&lt;p&gt;Qt 是 Qt 公司開發的開源專案，參閱 &lt;a href=&quot;http://%3/&quot;&gt;%3&lt;/a&gt; 取得更多資訊。&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
     <name>MAC_APPLICATION_MENU</name>
     <message>
         <source>Services</source>
@@ -265,7 +301,7 @@
     <message>
         <location filename="../../components/resultlistmodel.cpp" line="38"/>
         <source>Start typing to search for words</source>
-        <translation>開始輸入嚟查字典</translation>
+        <translation>開始輸入並搜尋</translation>
     </message>
 </context>
 <context>
@@ -412,7 +448,7 @@
     <message>
         <location filename="../../windows/updatewindow.cpp" line="68"/>
         <source>Show Details</source>
-        <translation>显示详细信息</translation>
+        <translation>顯示詳細資料…</translation>
     </message>
     <message>
         <location filename="../../windows/updatewindow.cpp" line="72"/>
@@ -422,12 +458,12 @@
     <message>
         <location filename="../../windows/updatewindow.cpp" line="100"/>
         <source>Hide details</source>
-        <translation>隐藏详细信息</translation>
+        <translation>隱藏詳細資料…</translation>
     </message>
     <message>
         <location filename="../../windows/updatewindow.cpp" line="119"/>
         <source>Show details</source>
-        <translation>显示详细信息</translation>
+        <translation>顯示詳細資料…</translation>
     </message>
 </context>
 </TS>
