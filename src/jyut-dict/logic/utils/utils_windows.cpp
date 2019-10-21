b22114c0ffa977512e863fe6ae50f6e9d6f77901
Aaron Tan <hi@aaronhktan.com>
2019-10-20 20:53:09 -0400

    Add missing files
    - Add two files

---
+++ src/jyut-dict/logic/utils/utils_windows.cpp
@@ -0,0 +1,10 @@
+#include "logic/utils/utils_windows.h"
+
+namespace Utils {
+
+    bool isDarkMode()
+    {
+        return true;
+    }
+
+}
---
+++ src/jyut-dict/logic/utils/utils_windows.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_WINDOWS_H
+#define UTILS_WINDOWS_H
+
+namespace Utils {
+
+#ifdef Q_OS_WIN
+    bool isDarkMode();
+#endif
+
+}
+
+#endif // UTILS_WINDOWS_H
