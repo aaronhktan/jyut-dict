3e4c220a174a9397a348c8afbd8e2a1f636f7f18
Aaron Tan <hi@aaronhktan.com>
2020-04-30 22:25:42 -0400

    Modify location of direct/indirect boolean and fix up tables

--- src/tatoeba_to_sqlite/tatoeba.py
+++ src/tatoeba_to_sqlite/tatoeba.py
@@ -1,7 +1,9 @@
 import hanzidentifier
 from hanziconv import HanziConv
 from pypinyin import pinyin, Style
+import pinyin_jyutping_sentence
 
+import collections
 import sqlite3
 import sys
 
@@ -26,14 +28,12 @@ class ChineseSentence(object):
         self.language = lang
 
 class NonChineseSentence(object):
-    def __init__(self, sentence_id=0, sentence='', lang='', direct=True):
+    def __init__(self, sentence_id=0, sentence='', lang=''):
         self.id=sentence_id
         self.sentence = sentence
         self.lang = lang
-        self.direct = direct
 
-    def set_direct(self, direct):
-        self.direct = direct
+Link = collections.namedtuple('Link', ['target', 'direct'])
 
 def write(chinese_sentences, nonchinese_sentences, links, db_name):
     print("Writing to database file")
@@ -60,8 +60,6 @@ def write(chinese_sentences, nonchinese_sentences, links, db_name):
                     pinyin TEXT,
                     jyutping TEXT,
                     language TEXT,
-                    fk_source_id INTEGER,
-                    FOREIGN KEY(fk_source_id) REFERENCES sources(source_id),
                     UNIQUE(traditional, simplified, pinyin, jyutping) ON CONFLICT IGNORE
                 )''')
 
@@ -81,17 +79,17 @@ def write(chinese_sentences, nonchinese_sentences, links, db_name):
                     non_chinese_sentence_id INTEGER PRIMARY KEY,
                     sentence TEXT,
                     language TEXT,
-                    direct BOOLEAN,
                     fk_source_id INTEGER,
-                    FOREIGN KEY(fk_source_id) REFERENCES sources(source_id),
-                    UNIQUE(non_chinese_sentence_id, sentence) ON CONFLICT IGNORE
+                    FOREIGN KEY(fk_source_id) REFERENCES sources(source_id) ON DELETE CASCADE,
+                    UNIQUE(non_chinese_sentence_id, sentence, fk_source_id) ON CONFLICT IGNORE
                 )''')
 
     c.execute('''CREATE TABLE sentence_links(
                     fk_chinese_sentence_id INTEGER,
                     fk_non_chinese_sentence_id INTEGER,
-                    FOREIGN KEY(fk_chinese_sentence_id) REFERENCES sentences(chinese_sentence_id),
-                    FOREIGN KEY(fk_non_chinese_sentence_id) REFERENCES sentences(non_chinese_sentence_id)
+                    direct BOOLEAN,
+                    FOREIGN KEY(fk_chinese_sentence_id) REFERENCES sentences(chinese_sentence_id) ON UPDATE CASCADE,
+                    FOREIGN KEY(fk_non_chinese_sentence_id) REFERENCES nonchinese_sentences(non_chinese_sentence_id) ON DELETE SET NULL
                 )''')
 
     # Add source to tables
@@ -101,32 +99,32 @@ def write(chinese_sentences, nonchinese_sentences, links, db_name):
             source['update_url'], source['other']))
 
     # Add sentences to tables
-    def chinese_sentence_to_tuple(sentence, source_id):
+    def chinese_sentence_to_tuple(sentence):
         return (sentence.id, sentence.traditional, sentence.simplified,
-            sentence.pinyin, sentence.jyutping, sentence.language, source_id)
+            sentence.pinyin, sentence.jyutping, sentence.language)
 
     def non_chinese_sentence_to_tuple(sentence, source_id):
-        return (sentence.id, sentence.sentence, sentence.lang, sentence.direct,
-            source_id)
+        return (sentence.id, sentence.sentence, sentence.lang, source_id)
 
     for key in chinese_sentences:
         sentence = chinese_sentences[key]
-        c.execute('INSERT INTO chinese_sentences values (?,?,?,?,?,?,?)',
-            chinese_sentence_to_tuple(sentence, 1))
+        c.execute('INSERT INTO chinese_sentences values (?,?,?,?,?,?)',
+            chinese_sentence_to_tuple(sentence))
 
         c.execute('SELECT last_insert_rowid()')
         sentence_id = c.fetchone()[0]
 
     for key in nonchinese_sentences:
         sentence = nonchinese_sentences[key]
-        c.execute('INSERT INTO nonchinese_sentences values (?,?,?,?,?)',
+        c.execute('INSERT INTO nonchinese_sentences values (?,?,?,?)',
             non_chinese_sentence_to_tuple(sentence, 1))
 
     # Add links
     for source_id in links:
-        target_id = links[source_id]
-        c.execute('INSERT INTO sentence_links values (?,?)',
-            (source_id, target_id))
+        target_id = links[source_id].target
+        direct = links[source_id].direct
+        c.execute('INSERT INTO sentence_links values (?,?,?)',
+            (source_id, target_id, direct))
 
     # Create indices
     # c.execute('CREATE INDEX fk_chinese_sentence_id_index ON sentence_links(fk_chinese_sentence_id)')
@@ -162,8 +160,10 @@ def parse_sentence_file(filename, source, target, sentences,
                     simp = HanziConv.toSimplified(sentence)
                 pin = ' '.join(p[0] for p in pinyin(sentence,
                     style=Style.TONE3)).lower()
+                jyut = pinyin_jyutping_sentence.jyutping(trad,
+                    tone_numbers=True, spaces=True)
                 sentence_row = ChineseSentence(sentence_id=sentence_id,
-                    trad=trad, simp=simp, pin=pin, lang=lang)
+                    trad=trad, simp=simp, pin=pin, jyut=jyut, lang=lang)
 
                 sentences[sentence_id] = sentence_row
                 continue
@@ -179,7 +179,8 @@ def parse_sentence_file(filename, source, target, sentences,
 
 
 def parse_links_file(filename, sentences, nonchinese_sentences,
-    nonchinese_sentences_filtered, intermediate_ids, links):
+    chinese_sentences_filtered, nonchinese_sentences_filtered, intermediate_ids,
+    links):
     print("Parsing links file...")
 
     with open(filename, 'r', encoding='utf8') as f:
@@ -194,7 +195,8 @@ def parse_links_file(filename, sentences, nonchinese_sentences,
             second_id = split[1]
 
             if first_id in sentences and second_id in nonchinese_sentences:
-                links[first_id] = second_id
+                links[first_id] = Link(second_id, True)
+                chinese_sentences_filtered[first_id] = chinese_sentences[first_id]
                 nonchinese_sentences_filtered[second_id] = nonchinese_sentences[second_id]
 
         # Also add translations of translations
@@ -203,7 +205,6 @@ def parse_links_file(filename, sentences, nonchinese_sentences,
         second_intermediate = {}
         f.seek(0, 0)
         for line in f:
-            i += 1
             if (len(line) == 0 or line[0] == '#'):
                 continue
 
@@ -224,8 +225,8 @@ def parse_links_file(filename, sentences, nonchinese_sentences,
             if key in second_intermediate:
                 source_id = first_intermediate[key]
                 target_id = second_intermediate[key]
-                links[source_id] = target_id
-                nonchinese_sentences[target_id].set_direct(False)
+                links[source_id] = Link(target_id, False)
+                chinese_sentences_filtered[source_id] = chinese_sentences[source_id]
                 nonchinese_sentences_filtered[target_id] = nonchinese_sentences[target_id]
 
 if __name__ == '__main__':
@@ -237,6 +238,7 @@ if __name__ == '__main__':
     chinese_sentences = {}     # Use this to store all the source sentences
     nonchinese_sentences = {}  # Use this to store all the target sentences
     intermediate_ids = set() # Use this to store ids of sentences between source/target
+    chinese_sentences_filtered = {} # Store only source sentences that match a target sentence
     nonchinese_sentences_filtered = {} # Store only target sentences that match a source sentence
     links = {}                 # Use this to store all the links between sentences
     source['name'] = sys.argv[6]
@@ -250,5 +252,7 @@ if __name__ == '__main__':
     parse_sentence_file(sys.argv[2], sys.argv[4], sys.argv[5],
         chinese_sentences, nonchinese_sentences, intermediate_ids)
     parse_links_file(sys.argv[3], chinese_sentences, nonchinese_sentences,
-        nonchinese_sentences_filtered, intermediate_ids, links)
-    write(chinese_sentences, nonchinese_sentences_filtered, links, sys.argv[1])
+        chinese_sentences_filtered, nonchinese_sentences_filtered,
+        intermediate_ids, links)
+    write(chinese_sentences_filtered, nonchinese_sentences_filtered, links,
+        sys.argv[1])
