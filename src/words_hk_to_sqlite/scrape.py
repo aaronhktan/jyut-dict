49d2aacc7ffaa0b9f316580dad06f7fc29a04479
Aaron Tan <hi@aaronhktan.com>
2020-12-05 02:54:16 -0500

    Move words.hk script
    - Added a few comments to explain how it works.

--- src/words_hk_to_sqlite/scrape.py
+++ src/words_hk_to_sqlite/scrape.py
@@ -16,16 +16,19 @@ for item in read_csv(sys.argv[1]):
   if (index < int(sys.argv[2])):
     continue 
 
+  # Enables login data to be persisted (to access words.hk)
   options = webdriver.ChromeOptions();
   options.add_argument("--user-data-dir=selenium")
 
   driver = webdriver.Chrome(options=options)
   driver.get(f"https://words.hk/zidin/{item}")
 
+  # Save the webpage
   with open(f"./scraped/{item}.html", "w") as f:
     f.write(driver.page_source)
 
   driver.quit()
 
+  # Be respectful whe scraping, hit only once every ten seconds!
   time.sleep(10)
 
