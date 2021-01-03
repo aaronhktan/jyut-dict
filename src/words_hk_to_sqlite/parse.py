b23e8ed96e5786d1240b0c839ed4db899dbfcbce
Aaron Tan <hi@aaronhktan.com>
2021-01-03 17:45:48 -0500

    Separate stuff into more functions
    - The old way of organizing things made big chungus functions. These smaller functions should be easier to understand and read.
    - Also combined code for the phrases/sentences, since they have the same structure.

--- src/words_hk_to_sqlite/parse.py
+++ src/words_hk_to_sqlite/parse.py
@@ -11,6 +11,8 @@ import os
 import sqlite3
 import sys
 
+# Structure of entries: a WORD contains multiple MEANINGS
+# A MEANING contains multiple DEFINITIONS in different languages, as well as EXAMPLES
 ExampleTuple = namedtuple('ExampleTuple', ['lang', 'pron', 'content'])
 DefinitionTuple = namedtuple('DefinitionTuple', ['lang', 'content'])
 MeaningTuple = namedtuple('Meaning', ['definitions', 'examplephrases', 'examplesentences'])
@@ -32,14 +34,7 @@ SourceTuple = namedtuple('Source',
 #   - 印 for item with different POS for different meanings
 #   - 使勁 for broken sentence
 
-def write(db_name, source, words):
-  print('Writing to database file')
-
-  db = sqlite3.connect(db_name)
-  c = db.cursor()
-
-  c.execute('PRAGMA user_version=3')
-
+def drop_tables(c):
   # Drop existing tables
   c.execute('DROP TABLE IF EXISTS entries')
   c.execute('DROP TABLE IF EXISTS entries_fts')
@@ -56,6 +51,7 @@ def write(db_name, source, words):
 
   c.execute('DROP TABLE IF EXISTS definitions_chinese_sentences_links')
 
+def create_tables(c):
   # Create new tables
   c.execute('''CREATE TABLE entries(
                   entry_id INTEGER PRIMARY KEY,
@@ -126,19 +122,81 @@ def write(db_name, source, words):
                   UNIQUE(fk_definition_id, fk_chinese_sentence_id) ON CONFLICT IGNORE
             )''')
 
-  # Add source information to table
-  c.execute('INSERT INTO sources values(?,?,?,?,?,?,?,?,?)',
-              (None, source.name, source.shortname, source.version,
-              source.description, source.legal, source.link,
-              source.update_url, source.other))
+# The example should be a list of ExampleTuples, such that
+# the first item is the 'source', and all subsequent ones are the translations
+def insert_example(c, definition_id, example):
+  trad = example[0].content
+  simp = HanziConv.toSimplified(trad)
+  jyut = example[0].pron
+  pin = ' '.join(lazy_pinyin(
+                    trad,
+                    style=Style.TONE3,
+                    neutral_tone_with_five=True)
+                ).lower().replace('v', 'u:')
+  lang = example[0].lang
 
-  # Loop through each entry
+  c.execute('SELECT max(rowid) FROM chinese_sentences')
+  before_example_id = c.fetchone()[0]
+  c.execute('INSERT INTO chinese_sentences values (?,?,?,?,?,?)',
+                (None, trad, simp, pin, jyut, lang))
+  c.execute('SELECT max(rowid) FROM chinese_sentences')
+  after_example_id = c.fetchone()[0]
+
+  # Check if example insertion was successful
+  if before_example_id == after_example_id:
+    if trad == 'X' or trad == 'x':
+      # Ignore examples that are just 'x'
+      return
+    else:
+      # If insertion failed, it's probably because the example already exists
+      # Get its rowid, so we can link it to this definition
+      c.execute('''SELECT rowid FROM chinese_sentences WHERE traditional=?
+                    AND simplified=? AND pinyin=? AND jyutping=? AND language=?''',
+                  (trad, simp, pin, jyut, lang))
+      row = c.fetchone()
+      if row is None:
+        logging.warning(f'Unexpected failure on insertion for example: {definition_id} {trad}')
+        return
+      example_id = row[0]
+  else:
+    example_id = after_example_id
+
+  c.execute('INSERT INTO definitions_chinese_sentences_links values (?,?)', (definition_id, example_id))
+
+  for translation in example[1:]:
+    trad = translation.content
+    lang = translation.lang
+
+    # Check if translation already exists before trying to insert
+    # Only need to insert a translation if it does not already exist in the table
+    c.execute('SELECT rowid FROM nonchinese_sentences WHERE sentence=? AND language=?',
+                (trad, lang))
+    row = c.fetchone()
+
+    if row is None:
+      c.execute('INSERT INTO nonchinese_sentences values (?,?,?)', (None, trad, lang))
+      c.execute('SELECT max(rowid) FROM nonchinese_sentences')
+      translation_id = c.fetchone()[0]
+      c.execute('INSERT INTO sentence_links values (?,?,?,?)', (example_id, translation_id, 1, True))
+
+def generate_indices(c):
+  # Generate fts5 indices for entries and definitions
+  c.execute('INSERT INTO entries_fts (rowid, pinyin, jyutping) SELECT rowid, pinyin, jyutping FROM entries')
+  c.execute('INSERT INTO definitions_fts (rowid, fk_entry_id, definition) select rowid, fk_entry_id, definition FROM definitions')
+  
+  c.execute('CREATE INDEX fk_entry_id_index ON definitions(fk_entry_id)')
+
+def insert_words(c, words):
   for key in words:
     for entry in words[key]:
       trad = entry.word
       simp = HanziConv.toSimplified(trad)
       jyut = entry.pronunciation
-      pin = ' '.join(lazy_pinyin(trad, style=Style.TONE3, neutral_tone_with_five=True)).lower().replace('v', 'u:')
+      pin = ' '.join(lazy_pinyin(
+                        trad,
+                        style=Style.TONE3,
+                        neutral_tone_with_five=True)
+                    ).lower().replace('v', 'u:')
       freq = zipf_frequency(trad, 'zh')
 
       c.execute('INSERT INTO entries values (?,?,?,?,?,?)', (None, trad, simp, pin, jyut, freq))
@@ -163,110 +221,84 @@ def write(db_name, source, words):
         definition_id = c.fetchone()[0]
 
         # Insert examples for each meaning
-        if meaning.examplesentences:
-          for sentence in meaning.examplesentences:
-            trad = sentence[0].content
-            simp = HanziConv.toSimplified(trad)
-            jyut = sentence[0].pron
-            pin = ' '.join(lazy_pinyin(trad, style=Style.TONE3, neutral_tone_with_five=True)).lower().replace('v', 'u:')
-            lang = sentence[0].lang
+        for sentence in meaning.examplesentences:
+          insert_example(c, definition_id, sentence)
 
-            c.execute('SELECT max(rowid) FROM chinese_sentences')
-            before_sentence_id = c.fetchone()[0]
-            c.execute('INSERT INTO chinese_sentences values (?,?,?,?,?,?)', (None, trad, simp, pin, jyut, lang))
-            c.execute('SELECT max(rowid) FROM chinese_sentences')
-            after_sentence_id = c.fetchone()[0]
+        for phrase in meaning.examplephrases:
+          insert_example(c, definition_id, phrase)
 
-            # Check if sentence insertion was successful
-            if before_sentence_id == after_sentence_id:
-              if trad == 'X' or trad == 'x':
-                # Ignore sentences that are just 'x'
-                continue
-              else:
-                # If insertion failed, it's probably because the sentence already exists
-                # Get its rowid, so we can link it to this definition
-                c.execute('''SELECT rowid FROM chinese_sentences WHERE traditional=?
-                              AND simplified=? AND pinyin=? AND jyutping=? AND language=?''',
-                            (trad, simp, pin, jyut, lang))
-                row = c.fetchone()
-                if row is None:
-                  logging.warning(f'Unexpected failure on insertion for sentence: {definition_id} {trad}')
-                  continue
-                sentence_id = row[0]
-            else:
-              sentence_id = after_sentence_id
+def write(db_name, source, words):
+  print('Writing to database file')
 
-            c.execute('INSERT INTO definitions_chinese_sentences_links values (?,?)', (definition_id, sentence_id))
+  db = sqlite3.connect(db_name)
+  c = db.cursor()
 
-            for translation in sentence[1:]:
-              trad = translation.content
-              lang = translation.lang
+  c.execute('PRAGMA user_version=3')
 
-              # Check if translation already exists before trying to insert
-              # Only need to insert a translation if it does not already exist in the table
-              c.execute('SELECT rowid FROM nonchinese_sentences WHERE sentence=? AND language=?',
-                          (trad, lang))
-              row = c.fetchone()
+  drop_tables(c)
+  create_tables(c)
 
-              if row is None:
-                c.execute('INSERT INTO nonchinese_sentences values (?,?,?)', (None, trad, lang))
-                c.execute('SELECT max(rowid) FROM nonchinese_sentences')
-                translation_id = c.fetchone()[0]
-                c.execute('INSERT INTO sentence_links values (?,?,?,?)', (sentence_id, translation_id, 1, True))
+  # Add source information to table
+  c.execute('INSERT INTO sources values(?,?,?,?,?,?,?,?,?)',
+              (None, source.name, source.shortname, source.version,
+              source.description, source.legal, source.link,
+              source.update_url, source.other))
 
-        if meaning.examplephrases:
-          for phrase in meaning.examplephrases:
-            trad = phrase[0].content
-            simp = HanziConv.toSimplified(trad)
-            jyut = phrase[0].pron
-            pin = ' '.join(lazy_pinyin(trad, style=Style.TONE3, neutral_tone_with_five=True)).lower().replace('v', 'u:')
-            lang = phrase[0].lang
+  insert_words(c, words)
 
-            c.execute('SELECT max(rowid) FROM chinese_sentences')
-            before_phrase_id = c.fetchone()[0]
-            c.execute('INSERT INTO chinese_sentences values (?,?,?,?,?,?)', (None, trad, simp, pin, jyut, lang))
-            c.execute('SELECT max(rowid) FROM chinese_sentences')
-            after_phrase_id = c.fetchone()[0]
-
-            if before_phrase_id == after_phrase_id:
-              if trad == 'X' or trad == 'x':
-                continue
-              else:
-                c.execute('''SELECT rowid FROM chinese_sentences WHERE traditional=?
-                              AND simplified=? AND pinyin=? AND jyutping=? AND language=?''',
-                            (trad, simp, pin, jyut, lang))
-                row = c.fetchone()
-                if row is None:
-                  logging.warning(f'Unexpected failure on insertion for phrase: {definition_id} {trad}')
-                  continue
-                phrase_id = row[0]
-            else:
-              phrase_id = after_phrase_id
-
-            c.execute('INSERT INTO definitions_chinese_sentences_links values (?,?)', (definition_id, phrase_id))
-
-            for translation in phrase[1:]:
-              trad = translation.content
-              lang = translation.lang
-
-              c.execute('SELECT rowid FROM nonchinese_sentences WHERE sentence=? AND language=?',
-                          (trad, lang))
-              row = c.fetchone()
-
-              if row is None:
-                c.execute('INSERT INTO nonchinese_sentences values (?,?,?)', (None, trad, lang))
-                c.execute('SELECT max(rowid) FROM nonchinese_sentences')
-                translation_id = c.fetchone()[0]
-                c.execute('INSERT INTO sentence_links values (?,?,?,?)', (phrase_id, translation_id, 1, True))
-
-  # Generate fts5 indices for entries and definitions
-  c.execute('INSERT INTO entries_fts (rowid, pinyin, jyutping) SELECT rowid, pinyin, jyutping FROM entries')
-  c.execute('INSERT INTO definitions_fts (rowid, fk_entry_id, definition) select rowid, fk_entry_id, definition FROM definitions')
-  
-  c.execute('CREATE INDEX fk_entry_id_index ON definitions(fk_entry_id)')
+  generate_indices(c)
 
   db.commit()
   db.close()
+
+def parse_definitions(def_elem):
+  try:
+   # Extract the language of this definition, and then remove it from the tree
+   lang_tag = def_elem.find('span', class_='zi-item-lang', recursive=False)
+   lang = lang_tag.text.strip('()')
+   lang_tag.decompose()
+  except:
+   lang = 'yue' # Assume Cantonese definition if no lang tag found
+
+  # Get the textual content of the definition
+  content = def_elem.text.strip()
+
+  return DefinitionTuple(lang, content)
+
+def parse_examples(ex_elem, classname):
+  ex = []
+  ex_divs = ex_elem.find_all('div', class_=None, recursive=False)
+  for ex_div in ex_divs:
+    lang_tag = ex_div.find('span', class_='zi-item-lang')
+    lang = lang_tag.text.strip('()')
+    lang_tag.decompose()
+
+    pronunciation_tag = ex_div.find('span', class_=classname)
+    if pronunciation_tag:
+      pronunciation = pronunciation_tag.text.strip('()')
+      pronunciation_tag.decompose()
+    else:
+      pronunciation = ''
+
+    content = ex_div.text.strip()
+    ex.append(ExampleTuple(lang, pronunciation, content))
+  return ex
+
+def log_word(word):
+  # Logging for debug purposes
+  logging.debug(' '.join([word.word, word.pronunciation]))
+  for index, meaning in enumerate(word.meanings):
+    logging.debug(f'Meaning {index}:')
+    for definition in meaning.definitions:
+      logging.debug(' '.join(['\tDefinition:', definition.lang, definition.content]))
+    for i, phrases in enumerate(meaning.examplephrases):
+      logging.debug(f'\t\tPhrase {i}:')
+      for phrase in phrases:
+        logging.debug(' '.join(['\t\t\tPhrase: ', phrase.lang, phrase.content, phrase.pron]))
+    for i, sentences in enumerate(meaning.examplesentences):
+      logging.debug(f'\t\tSentence {i}:')
+      for sentence in sentences:
+        logging.debug(' '.join(['\t\t\tSentence:', sentence.lang, sentence.content, sentence.pron]))
 
 def parse_file(file_name, words):
   with open(file_name, 'r') as file:
@@ -310,79 +342,24 @@ def parse_file(file_name, words):
         # Each definition for one meaning is contained in a classless div
         def_divs = list_item.find_all('div', class_=None, recursive=False)
         for def_div in def_divs:
-          try:
-            # Extract the language of this definition, and then remove it from the tree
-            lang_tag = def_div.find('span', class_='zi-item-lang', recursive=False)
-            lang = lang_tag.text.strip('()')
-            lang_tag.decompose()
-          except:
-            lang = 'yue' # Assume Cantonese definition if no lang tag found
-
-          # Get the textual content of the definition
-          content = def_div.text.strip()
-
-          meaning.definitions.append(DefinitionTuple(lang, content))
+          meaning.definitions.append(parse_definitions(def_div))
 
         # Each example phrase for the definition is contained in a div with class zi-details-phrase-item
         exphr_elems = list_item.find_all('div', class_='zi-details-phrase-item', recursive=False)
         for exphr_elem in exphr_elems:
-          exphr = []
-          exphr_divs = exphr_elem.find_all('div', class_=None, recursive=False)
-          for exphr_div in exphr_divs:
-            lang_tag = exphr_div.find('span', class_='zi-item-lang')
-            lang = lang_tag.text.strip('()')
-            lang_tag.decompose()
-
-            pronunciation_tag = exphr_div.find('span', class_='zi-item-phrase-pronunciation')
-            if pronunciation_tag:
-              pronunciation = pronunciation_tag.text.strip('()')
-              pronunciation_tag.decompose()
-            else:
-              pronunciation = ''
-
-            content = exphr_div.text.strip()
-            exphr.append(ExampleTuple(lang, pronunciation, content))
-          meaning.examplephrases.append(exphr)
+          meaning.examplephrases.append(parse_examples(exphr_elem, 'zi-item-phrase-pronunciation'))
 
         # Each example sentence for the definition is contained in a div with class zi-details-example-item
         exsen_elems = list_item.find_all('div', class_='zi-details-example-item', recursive=False)
-        for exsen_elems in exsen_elems:
-          exsen = []
-          exsen_divs = exsen_elems.find_all('div', class_=None, recursive=False)
-          for exsen_div in exsen_divs:
-            lang_tag = exsen_div.find('span', class_='zi-item-lang')
-            lang = lang_tag.text.strip('()')
-            lang_tag.decompose()
-
-            pronunciation_tag = exsen_div.find('span', class_='zi-item-example-pronunciation')
-            if pronunciation_tag:
-              pronunciation = pronunciation_tag.text.strip('()')
-              pronunciation_tag.decompose()
-            else:
-              pronunciation = ''
-
-            content = exsen_div.text.strip()
-            exsen.append(ExampleTuple(lang, pronunciation, content))
-          meaning.examplesentences.append(exsen)
+        for exsen_elem in exsen_elems:
+          meaning.examplesentences.append(parse_examples(exsen_elem, 'zi-item-example-pronunciation'))
 
         meanings.append(meaning)
 
-      words[word].append(WordTuple(word, word_pronunciation, meanings))
+      word_tuple = WordTuple(word, word_pronunciation, meanings)
+      log_word(word_tuple)
 
-      # Logging for debug purposes
-      logging.debug(word, word_pronunciation)
-      for index, meaning in enumerate(meanings):
-        logging.debug(f'Meaning {index}:')
-        for definition in meaning.definitions:
-          logging.debug('\tDefinition: ', definition.lang, definition.content)
-        for i, phrases in enumerate(meaning.examplephrases):
-          logging.debug(f'\t\tPhrase {i}:')
-          for phrase in phrases:
-            logging.debug('\t\t\tPhrase: ', phrase.lang, phrase.content, phrase.pron)
-        for i, sentences in enumerate(meaning.examplesentences):
-          logging.debug(f'\t\tSentence {i}:')
-          for sentence in sentences:
-            logging.debug('\t\t\tSentence: ', sentence.lang, sentence.content, sentence.pron)
+      words[word].append(word_tuple)
 
 def parse_folder(folder_name, words):
   for index, entry in enumerate(os.scandir(folder_name)):
@@ -401,7 +378,7 @@ if __name__ == '__main__':
 
   source = SourceTuple(sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6],
                           sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
-  # logging.basicConfig(level='INFO')
+  # logging.basicConfig(level='DEBUG')
   parsed_words = defaultdict(list)
   parse_folder(sys.argv[2], parsed_words)
   # parse_file(sys.argv[2], parsed_words)
