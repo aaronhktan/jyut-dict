50d9d3fca26ff8a6fb9a2d3b797520dc97d3e798
Aaron Tan <hi@aaronhktan.com>
2021-07-19 04:23:48 -0400

    Add more to parsing
    - Mostly done!

--- src/dictionaries/cantodict/parse.py
+++ src/dictionaries/cantodict/parse.py
@@ -1,10 +1,90 @@
 from bs4 import BeautifulSoup, element
+from wordfreq import zipf_frequency
 
 from database import database, objects
 
+import logging
 import os
 import re
+import sqlite3
 import sys
+
+# Useful test pages:
+#   - 鼻哥 for multiple definitions on a single line
+#   - 一 for multiple definitions
+#   - 惡 for multiple pronunciations
+#   - 傾掂 for POS + multiple definitions on same line
+#   - 庶 for weirdness involving newlines and <br> tags
+#   - 福地 for lack of space between [1] and [Taoism/Daoism]
+#   - 蚯蚓 for something weird in the parsing
+#   - 蚺蛇 for nonstandard listing of pronunciations
+#   - 大使 for nonstandard listing of pronunciations
+
+illegal_strings = ("Default PoS:", "Additional PoS:")
+
+
+def write(db_name, source, entries):
+    db = sqlite3.connect(db_name)
+    c = db.cursor()
+
+    # Set version of database
+    database.write_database_version(c)
+
+    # Delete old tables and indices, then create new one
+    database.drop_tables(c)
+    database.create_tables(c)
+
+    # Add sources to table
+    database.insert_source(
+        c,
+        source.name,
+        source.shortname,
+        source.version,
+        source.description,
+        source.legal,
+        source.link,
+        source.update_url,
+        source.other,
+        None,
+    )
+
+    for entry in entries:
+        entry_id = database.get_entry_id(
+            c,
+            entry.traditional,
+            entry.simplified,
+            entry.pinyin,
+            entry.jyutping,
+            entry.freq,
+        )
+
+        if entry_id == -1:
+            entry_id = database.insert_entry(
+                c,
+                entry.traditional,
+                entry.simplified,
+                entry.pinyin,
+                entry.jyutping,
+                entry.freq,
+            )
+            if entry_id == -1:
+                logging.error(f"Could not insert word {entry.traditional}, uh oh!")
+                continue
+
+        for label, definition in entry.definitions:
+            definition_id = database.insert_definition(
+                c, definition, label, entry_id, 1, None
+            )
+            if definition_id == -1:
+                logging.error(
+                    f"Could not insert definition {definition} for word {entry.traditional}, uh oh!"
+                )
+                continue
+
+    database.generate_indices(c)
+
+    db.commit()
+    db.close()
 
 
 def parse_file(file_name, words):
@@ -12,27 +92,48 @@ def parse_file(file_name, words):
         soup = BeautifulSoup(file, "html.parser")
 
         # Extract the traditional and simplified forms
-        forms = [x.strip() for x in soup.find("td", class_="chinesebig").get_text().split(" / ")]
-        if len(forms) > 1:
-            trad = forms[0]
-            simp = forms[1]
-        else:
-            trad = simp = forms[0]
+        try:
+            forms = [
+                x.strip()
+                for x in soup.find("td", class_="chinesebig").get_text().split(" / ")
+            ]
+            if len(forms) > 1:
+                trad = forms[0].strip()
+                simp = forms[1].strip()
+            else:
+                trad = simp = forms[0].strip()
+        except:
+            logging.error(
+                f"Couldn't find traditional and simplified forms in file {file_name}"
+            )
+            return
 
         word = os.path.splitext(os.path.basename(file_name))[0]
         if trad != word:
-            print(
-                f"Hmm, looks like the parsed word {trad} doesn't match the filename {word}"
-            )
+            if len(trad) == len(word):
+                logging.debug(
+                    f"{trad} and {word} have the same length, is this a simplified/traditional mixup?"
+                )
+                return
+            else:
+                logging.warning(
+                    f"Hmm, looks like the parsed word {trad} doesn't match the filename {word}"
+                )
+                return
+
+        freq = zipf_frequency(trad, "zh")
 
         # Extract the pronunciations
+        # CantoDict indicates differences in literary/colloquial pronunciation with *, but we don't support that
+        # So remove the stars
         jyut_element = soup.find("span", class_="cardjyutping")
         jyut = jyut_element.get_text() if jyut_element else ""
+        jyut = re.sub(r"\d\*", "", jyut)
 
         pin_element = soup.find("span", class_="cardpinyin")
         pin = pin_element.get_text() if pin_element else ""
 
-        # Extract the meaning(s)
+        # Extract the meaning element
         meaning_element = soup.find("td", class_="wordmeaning")
         # The layout of compound word pages is different from single-character pages
         real_meaning_element = meaning_element.find("div", class_=None)
@@ -45,16 +146,62 @@ def parse_file(file_name, words):
         for child in children:
             child.decompose()
 
+        # Parse the meanings from the meaning element
         meanings = []
         if meaning_element:
-            for string in re.compile(r"\[\d+\]\s+|\n").split(meaning_element.get_text()):
+            # CantoDict puts some weird stuff in the meanings div, and the only way to separate
+            # them out is to replace the <br> tags with "\n"
+            for br in soup.find_all("br"):
+                br.replace_with("\n")
+
+            strings = re.compile(r"\[\d+\]\s?|\n").split(meaning_element.get_text())
+            for string in strings:
                 string = string.strip()
-                if string and "Default PoS:" not in string:
-                    meanings.append(string)
+                if not string or any([x in string for x in illegal_strings]):
+                    continue
 
-        entry = objects.Entry(trad, simp, pin, jyut, defs=meanings)
+                # Check if this line is a line that denotes a specific pronunciation
+                result = re.search(r"\[粵\]\s+(.+\d+)\s+\|\s+\[國\]\s+(.+\d+)", string)
+                if result:
+                    # If there are previous definitions, add them to the words dict now
+                    # Since those meanings belong to the previous pronunciation
+                    if meanings:
+                        entry = objects.Entry(
+                            trad,
+                            simp,
+                            pin,
+                            jyut,
+                            freq=freq,
+                            defs=meanings,
+                        )
+                        words.append(entry)
 
-        print(entry)
+                    # Then, extract the new pinyin and jyutping
+                    # and reset the meanings tuple
+                    jyut = result.group(1)
+                    jyut = re.sub(r"\d\*", "", jyut)
+                    pin = result.group(2)
+                    meanings = []
+                    continue
+
+                # Try to isolate a label (usually a POS or [華]: indicating Mandarin-only usage or [粵]: indicating Cantonese-only usage)
+                label = ""
+                definition = string
+                result = re.search(r"^\[(.*?)\]:?\s+(.*)", string)
+                if result:
+                    label = result.group(1).strip()
+                    definition = result.group(2).strip()
+                else:
+                    # Filter out bad non-standard strings
+                    if string[0] == "[" and string[-1] == "]":
+                        continue
+                meanings.append((label, definition))
+
+        if meanings:
+            entry = objects.Entry(
+                trad, simp, pin, jyut, freq=freq, defs=meanings
+            )
+            words.append(entry)
 
 
 def parse_folder(folder_name, words):
@@ -65,5 +212,37 @@ def parse_folder(folder_name, words):
             parse_file(entry.path, words)
 
 
-words = {}
-parse_folder(sys.argv[1], {})
+if __name__ == "__main__":
+    if len(sys.argv) != 11:
+        print(
+            (
+                "Usage: python3 script.py <database filename> "
+                "<HTML folder> <source name> <source short name> "
+                "<source version> <source description> <source legal> "
+                "<source link> <source update url> <source other>"
+            )
+        )
+        print(
+            (
+                "e.g. python3 script.py cantodict.db scraped/data/ CantoDict CD 2021-07-18 "
+                '"CantoDict is a collaborative Chinese Dictionary project started in November 2003. '
+                'Entries are added and mistakes corrected by a team of kind volunteers from around the world." '
+                '"https://www.cantonese.sheik.co.uk/copyright.htm" "https://www.cantonese.sheik.co.uk/" "" ""'
+            )
+        )
+        sys.exit(1)
+
+    source = objects.SourceTuple(
+        sys.argv[3],
+        sys.argv[4],
+        sys.argv[5],
+        sys.argv[6],
+        sys.argv[7],
+        sys.argv[8],
+        sys.argv[9],
+        sys.argv[10],
+    )
+
+    words = []
+    parse_folder(sys.argv[2], words)
+    write(sys.argv[1], source, words)
