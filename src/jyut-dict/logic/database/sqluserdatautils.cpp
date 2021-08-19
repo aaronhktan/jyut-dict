0c7142e5243c9b2ed1d31d3d6e465ad0c7244bbe
Aaron Tan <hi@aaronhktan.com>
2021-08-19 19:31:31 -0400

    Fix querying for favourites
    - After the query parsing change, the favourites query was not updated. This commit updates it to use the new JSON format.

--- src/jyut-dict/logic/database/sqluserdatautils.cpp
+++ src/jyut-dict/logic/database/sqluserdatautils.cpp
@@ -94,31 +94,99 @@ void SQLUserDataUtils::searchForAllFavouritedWordsThread(void)
 
     QSqlQuery query{_manager->getDatabase()};
 
-    query.exec("DROP TABLE IF EXISTS favourite_tmp");
-    // Create a table with two columns, the entry_id and its associated
-    // timestamp.
-    query.exec("CREATE TEMPORARY TABLE favourite_tmp AS "
-               "SELECT entry_id, timestamp from entries "
-               "INNER JOIN user.favourite_words "
-               " ON entries.simplified = favourite_words.simplified "
-               " AND entries.traditional = favourite_words.traditional "
-               " AND entries.jyutping = favourite_words.jyutping "
-               " AND entries.pinyin = favourite_words.pinyin");
     // Select the fields of the entry and its definitions from the entry_id
     // from the temporary table, then sort by the timestamp.
     query.exec(
-        "SELECT entries.traditional, entries.simplified, entries.pinyin, "
-        " entries.jyutping, "
-        " group_concat(sourcename || ' ' || definition, '●') AS "
-        " definitions, timestamp "
-        "FROM entries, definitions, sources, favourite_tmp "
-        "WHERE entries.entry_id IN "
-        "(SELECT entry_id FROM favourite_tmp) "
-        "AND favourite_tmp.entry_id = entries.entry_id "
-        "AND entries.entry_id = fk_entry_id "
-        "AND source_id = fk_source_id "
-        "GROUP BY entries.entry_id "
-        "ORDER BY timestamp DESC");
+        //// Get list of entry ids whose simplified form matches the query
+        //// This CTE is used twice; would be nice if could materialize it
+        "WITH matching_entry_ids AS ( "
+        "  SELECT entry_id, timestamp from entries "
+        "  INNER JOIN user.favourite_words "
+        "    ON entries.simplified = favourite_words.simplified "
+        "    AND entries.traditional = favourite_words.traditional "
+        "    AND entries.jyutping = favourite_words.jyutping "
+        "    AND entries.pinyin = favourite_words.pinyin "
+        "), "
+        " "
+        //// Get the list of all definitions for those entries
+        //// This CTE is used twice; would be nice if could materialize it
+        "matching_definition_ids AS ( "
+        "  SELECT definition_id, definition FROM definitions WHERE fk_entry_id IN ( "
+        "       SELECT entry_id FROM matching_entry_ids "
+        "  ) "
+        "), "
+        " "
+        //// Get corresponding sentence ids for each of those definitions
+        //// This CTE is used twice; would be nice if could materialize it
+        "matching_chinese_sentence_ids AS ( "
+        "  SELECT definition_id, fk_chinese_sentence_id "
+        "  FROM matching_definition_ids AS mdi "
+        "  JOIN definitions_chinese_sentences_links AS dcsl ON mdi.definition_id = dcsl.fk_definition_id "
+        "), "
+        " "
+        //// Get translations for each of those sentences
+        "matching_translations AS ( "
+        "  SELECT mcsi.fk_chinese_sentence_id, "
+        "    json_group_array(DISTINCT "
+        "      json_object('sentence', sentence, "
+        "                  'language', language, "
+        "                  'direct', direct "
+        "    )) AS translation "
+        "  FROM matching_chinese_sentence_ids AS mcsi "
+        "  JOIN sentence_links AS sl ON mcsi.fk_chinese_sentence_id = sl.fk_chinese_sentence_id "
+        "  JOIN nonchinese_sentences AS ncs ON ncs.non_chinese_sentence_id = sl.fk_non_chinese_sentence_id "
+        "  GROUP BY mcsi.fk_chinese_sentence_id "
+        "), "
+        " "
+        //// Create sentence object for each sentence
+        "matching_sentences AS ( "
+        "  SELECT chinese_sentence_id, "
+        "    json_object('traditional', traditional, "
+        "                'simplified', simplified, "
+        "                'pinyin', pinyin, "
+        "                'jyutping', jyutping, "
+        "                'language', language, "
+        "                'translations', json(translation)) AS sentence "
+        "  FROM chinese_sentences AS cs "
+        "  LEFT JOIN matching_translations AS mt ON cs.chinese_sentence_id = mt.fk_chinese_sentence_id "
+        "), "
+        " "
+        //// Create definition object for each definition
+        "matching_definitions AS ( "
+        "  SELECT definitions.fk_entry_id, definitions.fk_source_id, "
+        "    json_object('definition', definitions.definition, "
+        "                'label', label, 'sentences', "
+        "                json_group_array(json(sentence))) AS definition "
+        "  FROM matching_definition_ids AS mdi "
+        "  LEFT JOIN definitions ON mdi.definition_id = definitions.definition_id "
+        "  LEFT JOIN matching_chinese_sentence_ids AS mcsi ON mdi.definition_id = mcsi.definition_id "
+        "  LEFT JOIN matching_sentences AS ms ON mcsi.fk_chinese_sentence_id = ms.chinese_sentence_id "
+        "  GROUP BY mdi.definition_id "
+        "), "
+        " "
+        //// Create definition groups for definitions of the same entry that come from the same source
+        "matching_definition_groups AS ( "
+        "  SELECT fk_entry_id, "
+        "    json_object('source', sourcename, "
+        "                'definitions', "
+        "                json_group_array(json(definition))) AS definitions "
+        "  FROM matching_definitions AS md "
+        "  LEFT JOIN sources ON sources.source_id = md.fk_source_id "
+        "  GROUP BY fk_entry_id, fk_source_id "
+        "), "
+        " "
+        //// Construct the final entry object
+        "matching_entries AS ( "
+        "  SELECT simplified, traditional, jyutping, pinyin, json_group_array(json(definitions)) AS definitions "
+        "  FROM matching_definition_groups AS mdg "
+        "  LEFT JOIN entries ON entries.entry_id = mdg.fk_entry_id "
+        "  LEFT JOIN matching_entry_ids AS mei ON entries.entry_id = mei.entry_id "
+        "  GROUP BY entries.entry_id "
+        "  ORDER BY timestamp DESC "
+        ") "
+        " "
+        "SELECT simplified, traditional, jyutping, pinyin, definitions from matching_entries"
+    );
 
     results = QueryParseUtils::parseEntries(query);
     _manager->closeDatabase();
