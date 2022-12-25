0e9dbc2b84ba8360c9f38c6cd6964896c9d3ddb3
Aaron Tan <hi@aaronhktan.com>
2022-12-25 07:29:05 +0000

    Fix compilation on Windows
    - Fixed compilation on Windows.
    - Also fixed weird JP/PY/YL/ZY resizing bug on Windows when switching
      between light/dark mode.

--- src/jyut-dict/components/entryview/entryheaderwidget.cpp
+++ src/jyut-dict/components/entryview/entryheaderwidget.cpp
@@ -55,8 +55,8 @@ void EntryHeaderWidget::changeEvent(QEvent *event)
 {
 #ifdef Q_OS_WIN
     if (event->type() == QEvent::FontChange) {
-        _jyutpingLabel->setFixedWidth(_jyutpingLabel->fontMetrics().boundingRect("JP").width());
-        _pinyinLabel->setFixedWidth(_pinyinLabel->fontMetrics().boundingRect("PY").width());
+        // This is just to correctly resize the JP/PY etc. labels
+        translateUI();
     }
 #endif
     if (event->type() == QEvent::PaletteChange && !_paletteRecentlyChanged) {
@@ -210,7 +210,6 @@ void EntryHeaderWidget::setStyle(bool use_dark)
                                           LABEL_TEXT_COLOUR_LIGHT_R};
     for (const auto& label : _pronunciationTypeLabels) {
         label->setAttribute(Qt::WA_TranslucentBackground);
-        label->setVisible(false);
         label->setStyleSheet(styleSheet.arg(textColour.name()));
     }
 
