c92ba0cf4fbdf1c286883d9f881f064441ed49ce
Aaron Tan <hi@aaronhktan.com>
2024-03-13 22:19:35 -0400

    Remove static variable declaration in header file
    - The map name_to_short_name was declared static in a header file
      before. This meant that every translation unit would have its own
      copy of the vector. Not sure why it ever worked before, since the
      vector was only populated in main.cpp...
    - Also added synchronization primitives for the unordered_map.

--- src/jyut-dict/logic/dictionary/dictionarysource.cpp
+++ src/jyut-dict/logic/dictionary/dictionarysource.cpp
@@ -1,9 +1,16 @@
 #include "logic/dictionary/dictionarysource.h"
 
+#include <algorithm>
+#include <shared_mutex>
+
 namespace DictionarySourceUtils {
+
+std::unordered_map<std::string, std::string> name_to_short_name;
+std::shared_mutex map_mutex;
 
 std::string getSourceShortString(const std::string &source)
 {
+    std::shared_lock lock{map_mutex};
     auto result = name_to_short_name.find(source);
 
     if (result == name_to_short_name.end()) {
@@ -15,17 +22,19 @@ std::string getSourceShortString(const std::string &source)
 
 bool addSource(const std::string &sourcename, const std::string &shortsourcename)
 {
+    std::lock_guard lock{map_mutex};
     return name_to_short_name.insert({sourcename, shortsourcename}).second;
 }
 
 bool removeSource(const std::string &sourcename)
 {
+    std::lock_guard lock{map_mutex};
     auto index = std::find_if(name_to_short_name.begin(),
-                           name_to_short_name.end(),
-                           [sourcename](
-                               std::pair<std::string, std::string> source) {
-                               return source.first == sourcename;
-                           });
+                              name_to_short_name.end(),
+                              [sourcename](
+                                  std::pair<std::string, std::string> source) {
+                                  return source.first == sourcename;
+                              });
     if (index == name_to_short_name.end()) {
         return false;
     }
