9dc2e4c14801f5c25300609a1aba429cff8b0b02
Aaron Tan <hi@aaronhktan.com>
2025-06-01 23:38:21 -0400

    Translate keyboard shortcut text

--- src/jyut-dict/resources/translations/jyutdictionary-zh_Hant.ts
+++ src/jyut-dict/resources/translations/jyutdictionary-zh_Hant.ts
@@ -484,31 +484,31 @@
     <name>MAC_APPLICATION_MENU</name>
     <message>
         <source>Services</source>
-        <translation>服務</translation>
+        <translation type="vanished">服務</translation>
     </message>
     <message>
         <source>Hide %1</source>
-        <translation>隱藏 %1</translation>
+        <translation type="vanished">隱藏 %1</translation>
     </message>
     <message>
         <source>Hide Others</source>
-        <translation>隱藏其他</translation>
+        <translation type="vanished">隱藏其他</translation>
     </message>
     <message>
         <source>Show All</source>
-        <translation>顯示全部</translation>
+        <translation type="vanished">顯示全部</translation>
     </message>
     <message>
         <source>Preferences...</source>
-        <translation>偏好設定…</translation>
+        <translation type="vanished">偏好設定…</translation>
     </message>
     <message>
         <source>Quit %1</source>
-        <translation>結束 %1</translation>
+        <translation type="vanished">結束 %1</translation>
     </message>
     <message>
         <source>About %1</source>
-        <translation>關於 %1</translation>
+        <translation type="vanished">關於 %1</translation>
     </message>
 </context>
 <context>
@@ -710,6 +710,30 @@ Hang tight!</source>
         <source>Start Handwriting</source>
         <translation>開啟「手寫」</translation>
     </message>
+    <message>
+        <source>Disable Automatic Search Language Switching</source>
+        <translation>停用自動偵測搜尋語言</translation>
+    </message>
+    <message>
+        <source>Enable Automatic Search Language Switching</source>
+        <translation>啟用自動偵測搜尋語言</translation>
+    </message>
+    <message>
+        <source>Disable Fuzzy Jyutping</source>
+        <translation>停用模糊粵拼</translation>
+    </message>
+    <message>
+        <source>Enable Fuzzy Jyutping</source>
+        <translation>啟用模糊粵拼</translation>
+    </message>
+    <message>
+        <source>Disable Fuzzy Pinyin</source>
+        <translation>停用模糊漢語拼音</translation>
+    </message>
+    <message>
+        <source>Enable Fuzzy Pinyin</source>
+        <translation>啟用模糊漢語拼音</translation>
+    </message>
 </context>
 <context>
     <name>NoUpdateDialog</name>
@@ -739,23 +763,23 @@ Hang tight!</source>
     <name>QMessageBox</name>
     <message>
         <source>Show Details...</source>
-        <translation>顯示詳情...</translation>
+        <translation type="vanished">顯示詳情...</translation>
     </message>
     <message>
         <source>Hide Details...</source>
-        <translation>隱藏詳情...</translation>
+        <translation type="vanished">隱藏詳情...</translation>
     </message>
     <message>
         <source>&lt;h3&gt;About Qt&lt;/h3&gt;&lt;p&gt;This program uses Qt version %1.&lt;/p&gt;</source>
-        <translation>&lt;h3&gt;關於 Qt&lt;/h3&gt;&lt;p&gt;此程式使用 Qt %1 版。&lt;/p&gt;</translation>
+        <translation type="vanished">&lt;h3&gt;關於 Qt&lt;/h3&gt;&lt;p&gt;此程式使用 Qt %1 版。&lt;/p&gt;</translation>
     </message>
     <message>
         <source>About Qt</source>
-        <translation>關於 Qt</translation>
+        <translation type="vanished">關於 Qt</translation>
     </message>
     <message>
         <source>&lt;p&gt;Qt is a C++ toolkit for cross-platform application development.&lt;/p&gt;&lt;p&gt;Qt provides single-source portability across all major desktop operating systems. It is also available for embedded Linux and other embedded and mobile operating systems.&lt;/p&gt;&lt;p&gt;Qt is available under three different licensing options designed to accommodate the needs of our various users.&lt;/p&gt;&lt;p&gt;Qt licensed under our commercial license agreement is appropriate for development of proprietary/commercial software where you do not want to share any source code with third parties or otherwise cannot comply with the terms of the GNU LGPL version 3.&lt;/p&gt;&lt;p&gt;Qt licensed under the GNU LGPL version 3 is appropriate for the development of Qt&amp;nbsp;applications provided you can comply with the terms and conditions of the GNU LGPL version 3.&lt;/p&gt;&lt;p&gt;Please see &lt;a href=&quot;http://%2/&quot;&gt;%2&lt;/a&gt; for an overview of Qt licensing.&lt;/p&gt;&lt;p&gt;Copyright (C) %1 The Qt Company Ltd and other contributors.&lt;/p&gt;&lt;p&gt;Qt and the Qt logo are trademarks of The Qt Company Ltd.&lt;/p&gt;&lt;p&gt;Qt is The Qt Company Ltd product developed as an open source project. See &lt;a href=&quot;http://%3/&quot;&gt;%3&lt;/a&gt; for more information.&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Qt 是個用於跨平台應用程式開發的工具箱。&lt;/p&gt;
+        <translation type="vanished">&lt;p&gt;Qt 是個用於跨平台應用程式開發的工具箱。&lt;/p&gt;
 &lt;p&gt;Qt 提供了可移植單一來源以能在目前主流的桌面作業系統跨平台。其也支援嵌入式 Linux 和其他嵌入和行動作業系統。&lt;/p&gt;
 &lt;p&gt;Qt 可以用於三個不同的授權選項，以接納各個使用者的需求。&lt;/p&gt;
 &lt;p&gt;使用我們的商業授權條款授權的 Qt 適合拿來開發不希望公開源碼給第三方的專有或商業軟體，或者是無法完全遵守 GNU LGPL 第三版條款的軟體。&lt;/p&gt;
@@ -770,15 +794,15 @@ Hang tight!</source>
     <name>QPlatformTheme</name>
     <message>
         <source>Cancel</source>
-        <translation>取消</translation>
+        <translation type="vanished">取消</translation>
     </message>
     <message>
         <source>&amp;Yes</source>
-        <translation>是</translation>
+        <translation type="vanished">是</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation>確定</translation>
+        <translation type="vanished">確定</translation>
     </message>
 </context>
 <context>
