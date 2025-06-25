efac7aef32994f90937afa51d9317dd2bb9b5934
Aaron Tan <hi@aaronhktan.com>
2025-06-24 22:32:12 -0400

    Fix intermittent crashing on Windows
    - Turns out qApp->setStyleSheet causes a crash when QMainWindow::show()
      is called.

--- src/jyut-dict/main.cpp
+++ src/jyut-dict/main.cpp
@@ -7,18 +7,15 @@
 
 #if defined(Q_OS_WIN)
 #include <Windows.h>
-#include <sapi.h>
 
 #include <cstdio>
 #include <cstring>
-
-constexpr auto ARG_STR_LEN = 64;
-constexpr auto platformArg = "-platform";
-constexpr auto darkModeArg = "windows:darkmode=1";
 #endif
 
 int main(int argc, char *argv[])
 {
+    QApplication a{argc, argv};
+
     QCoreApplication::setOrganizationName("Aaron Tan");
     QCoreApplication::setOrganizationDomain("aaronhktan.com");
 #ifdef APPIMAGE
@@ -50,28 +47,12 @@ int main(int argc, char *argv[])
     qRegisterMetaType<TextToSpeech::SpeakerVoice>();
 
 #if defined(Q_OS_WIN)
-    // This is kind of a horrible hack to get dark borders on Windows
-    // But it works!
-    int new_argc = argc + 2;
-    char **new_argv = static_cast<char **>(
-        malloc(sizeof(*new_argv) * (argc + 2)));
-    memcpy(new_argv, argv, argc * sizeof(*argv));
-    char new_platform_arg[ARG_STR_LEN];
-    snprintf(new_platform_arg, ARG_STR_LEN, "%s", platformArg);
-    new_argv[argc] = new_platform_arg;
-    char new_dark_mode_arg[ARG_STR_LEN];
-    snprintf(new_dark_mode_arg, ARG_STR_LEN, "%s", darkModeArg);
-    new_argv[argc + 1] = new_dark_mode_arg;
-
     HRESULT hr = CoInitialize(NULL);
     if (FAILED(hr)) {
         std::cerr << "Failed to initialize COM library." << std::endl;
         return -1;
     }
-
-    QApplication a{new_argc, new_argv};
-#else
-    QApplication a{argc, argv};
+    qputenv("QT_QPA_PLATFORM", "windows:darkmode=1");
 #endif
 
     MainWindow w;
